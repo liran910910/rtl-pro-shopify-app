e0f64649e38bb7c7773e6d36116e12de4f685edd
liran910910 <liran53@gmail.com>
2026-02-24 23:01:51 +0200

    Refactor Shopify app configuration and billing plans

--- web/shopify.js
+++ web/shopify.js
@@ -5,46 +5,44 @@ import sessionStoragePkg from "@shopify/shopify-app-session-storage-prisma";
 const { PrismaSessionStorage } = sessionStoragePkg;
 import prismaPkg from "@prisma/client";
 const { PrismaClient } = prismaPkg;
-import { restResources } from "@shopify/shopify-api/rest/admin/2026-01";
 
 const prisma = new PrismaClient();
 
 const shopify = shopifyApp({
-    api: {
-          apiVersion: "2026-01",
-          restResources,
-          billing: {
-                  // Basic Plan
-            "RTL Pro Basic": {
-                      amount: 7.75,
-                      currencyCode: "USD",
-                      interval: "EVERY_30_DAYS",
-                      trialDays: 5,
-            },
-                  // Pro Plan
-                  "RTL Pro Pro": {
-                            amount: 9.95,
+      api: {
+              apiVersion: "2026-01",
+              billing: {
+                        // Basic Plan
+                "RTL Pro Basic": {
+                            amount: 7.75,
                             currencyCode: "USD",
                             interval: "EVERY_30_DAYS",
                             trialDays: 5,
-                  },
-                  // Premium Plan
-                  "RTL Pro Premium": {
-                            amount: 14.75,
-                            currencyCode: "USD",
-                            interval: "EVERY_30_DAYS",
-                            trialDays: 5,
-                  },
-          },
-    },
-    auth: {
-          path: "/api/auth",
-          callbackPath: "/api/auth/callback",
-    },
-    webhooks: {
-          path: "/api/webhooks",
-    },
-    sessionStorage: new PrismaSessionStorage(prisma),
+                },
+                        // Pro Plan
+                        "RTL Pro Pro": {
+                                    amount: 9.95,
+                                    currencyCode: "USD",
+                                    interval: "EVERY_30_DAYS",
+                                    trialDays: 5,
+                        },
+                        // Premium Plan
+                        "RTL Pro Premium": {
+                                    amount: 14.75,
+                                    currencyCode: "USD",
+                                    interval: "EVERY_30_DAYS",
+                                    trialDays: 5,
+                        },
+              },
+      },
+      auth: {
+              path: "/api/auth",
+              callbackPath: "/api/auth/callback",
+      },
+      webhooks: {
+              path: "/api/webhooks",
+      },
+      sessionStorage: new PrismaSessionStorage(prisma),
 });
 
 export default shopify;
